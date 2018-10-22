6264ba62030afa0645f8be29261191aaa6611c5d
Yusuf Ogunjobi <1009097o@student.gla.ac.uk>
2018-10-22 14:05:57 +0100

    Q1 completed

--- src/main/java/answers/Question1.java
+++ src/main/java/answers/Question1.java
@@ -3,8 +3,28 @@ package answers;
 public class Question1 {
 
 	public static int bestMergedPortfolio(int[] portfolios) {
-		// TODO Auto-generated method stub
+		int maxEvaluationValue = 0;
+		int arraySize = portfolios.length;
+		int pMaxDecimalValue = (int) (Math.pow(2,16)-1);
+
+		if(arraySize<=100) {
+			
+			for (int i = 0; i < arraySize; i++) {
+				int portfolio = portfolios[i];
+				if(portfolio < 0 ||portfolio > pMaxDecimalValue)
+					return -1;
+				for (int j = i+1; j < arraySize; j++) {
+					maxEvaluationValue = Math.max(bitwiseXor(portfolio, portfolios[j]),maxEvaluationValue);
+				}
+			}
+			return maxEvaluationValue;
+		}
+		
+		
 		return -1;
 	}
-
+	
+	private static int bitwiseXor(int portfolio1, int portfolio2) {
+		return portfolio1^portfolio2;
+	}
 }
