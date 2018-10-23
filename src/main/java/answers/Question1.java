534931a7c765cc39b2aa4aadaf5f47c6a2a9f590
Yusuf Ogunjobi <1009097o@student.gla.ac.uk>
2018-10-23 13:12:46 +0100

    Q1 redone, boundary condition removed

--- src/main/java/answers/Question1.java
+++ src/main/java/answers/Question1.java
@@ -4,7 +4,6 @@ public class Question1 {
 
 	public static int bestMergedPortfolio(int[] portfolios) {
 		int arraySize = portfolios.length;
-		int pMaxDecimalValue = (int) (Math.pow(2,16)-1);
 
 	      if (arraySize<=100) {
 			if (arraySize < 2) {
@@ -12,9 +11,7 @@ public class Question1 {
 			}
 			int maxValue = 0;
 			for (int i = 0; i < 2; i++) {
-				if (portfolios[i] > pMaxDecimalValue) {
-					return -1;
-				}
+
 				maxValue = maxValue ^ portfolios[i];
 			}
 			int currentMaxValue = maxValue;
